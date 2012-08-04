09fe5350a8cd4080bbc72f8de3cb69f34b17cdce
llorentas <llorentas@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-08-04 06:13:58 +0000

    Tvarkymas
    git-svn-id: https://plugins.svn.wordpress.org/verisign-domain-hashlink/trunk@581617 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- domainhashlink-wordpressplugin.v1.0.5/components/code/dhl_search.php
+++
@@ -1,6 +0,0 @@
-search
-<div>
-	<!--<label for="dhl_search_box">Search for:</label>-->
-	<input type="text" value="" name="dhl_search_box" id="<?php echo $dhl_field; ?>">
-	<input type="submit" id="<?php echo $dhl_button; ?>" value="Search">
-</div>
--- domainhashlink-wordpressplugin.v1.0.5/views/faq.php
+++
@@ -1,2 +0,0 @@
-<h3><?php echo DHLOutput::_e('FAQ'); ?></h3>
-<iframe src="<?php echo DHL_FAQ; ?>" width="100%" height="600" />
